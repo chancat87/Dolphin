95f9066789758ee67f2c6a0f6859ab9f344cad1c
yangyang <mengyangyang@dataoceanai.com>
2026-03-11 11:52:34 +0800

    refactor load_model transcribe interface

--- dolphin/transcribe.py
+++ dolphin/transcribe.py
@@ -24,67 +24,31 @@ from pathlib import Path
 from argparse import Namespace
 from os.path import dirname, join, abspath, join
 from distutils.util import strtobool
-from typing import Union, Optional, Tuple, List
+from typing import Union, Optional, Tuple, List, Dict, Any
 
 import torch
+import modelscope
+from modelscope.models.audio.funasr.model import GenericFunASR
 try:
     import torch_npu
     torch_npu_is_imported = True
 except:
     torch_npu_is_imported = False
-import modelscope
-from modelscope.models.audio.funasr.model import GenericFunASR
 
-from .audio import load_audio, convert_audio
-from .model import DolphinSpeech2Text, TranscribeResult, TranscribeSegmentResult
-from .languages import LANGUAGE_REGION_CODES, LANGUAGE_CODES
-from .constants import SPEECH_LENGTH
+from dolphin.audio import load_audio, convert_audio
+# from dolphin.model import DolphinSpeech2Text, TranscribeResult, TranscribeSegmentResult
+from dolphin.processor import extract_feats
+from dolphin.model import (ASRModel, init_speech_model,
+                           TranscribeResult, TranscribeSegmentResult)
+from dolphin.languages import LANGUAGE_REGION_CODES, LANGUAGE_CODES
+from dolphin.constants import SPEECH_LENGTH
+from dolphin.model_registry import MODELS
+from dolphin.tokenizer import init_tokenizer, BaseTokenizer
 
 logger = logging.getLogger("dolphin")
 
 VAD_MODEL = "iic/speech_fsmn_vad_zh-cn-16k-common-pytorch"
 
-
-MODELS = {
-    "base": {
-        "model_id": "DataoceanAI/dolphin-base",
-        "download_url": "http://so-algorithm-prod.oss-cn-beijing.aliyuncs.com/models/dolphin/base.pt",
-        "sha256": "688f0cdb26da2684a4eec200a432091920287585e8e332507cbe9c1ab6d77401",
-        "config": {
-            "encoder": {
-                "output_size": 512,
-                "attention_heads": 8,
-                "cgmlp_linear_units": 2048,
-                "num_blocks": 6,
-                "linear_units": 2048,
-            },
-            "decoder": {
-                "attention_heads": 8,
-                "linear_units": 2048,
-                "num_blocks": 6,
-            }
-        }
-    },
-    "small": {
-        "model_id": "DataoceanAI/dolphin-small",
-        "download_url": "http://so-algorithm-prod.oss-cn-beijing.aliyuncs.com/models/dolphin/small.pt",
-        "sha256": "4a0c6c636657121ec2a2b656e97e45b29a8b29c92fa3998006e02ab146d8ac51",
-        "config": {
-            "encoder": {
-                "output_size": 768,
-                "attention_heads": 12,
-                "cgmlp_linear_units": 3072,
-                "num_blocks": 12,
-                "linear_units": 1536,
-            },
-            "decoder": {
-                "attention_heads": 12,
-                "linear_units": 3072,
-                "num_blocks": 12,
-            }
-        }
-    },
-}
 
 
 def str2bool(value: str) -> bool:
@@ -105,7 +69,6 @@ def parser_args() -> Namespace:
     parser.add_argument("--predict_time", type=str2bool, default=True, help="whether predict timestamp (default: true)")
     parser.add_argument("--beam_size", type=int, default=5, help="number of beams in beam search (default: 5)")
     parser.add_argument("--maxlenratio", type=float, default=0.0, help="Input length ratio to obtain max output length (default: 0.0)")
-    parser.add_argument("--disable_model_hash_check", action="store_true", help="Disable model checkpoint hash check")
 
     args = parser.parse_args()
     return args
@@ -140,72 +103,59 @@ def load_model(
     model_name: str,
     model_dir: Union[Path, str],
     device: Optional[Union[str, torch.device]] = None,
-    **kwargs
-) -> DolphinSpeech2Text:
+) -> ASRModel:
     """
-    Load DolphinSpeech2Text model.
+    Load model.
 
     Args:
         model_name: model name (e.g. small)
         model_dir: model download directory
-        device: the pytorch device
+        device: the accelerator device
 
     Returns:
-        DolphinSpeech2Text instance
+        ASRModel instance
     """
     if device is None:
         device = detect_device()
         logger.info(f"auto detect device: {device}")
 
-    model_config = kwargs.get("model_config") or MODELS[model_name]["config"]
-    train_cfg_file = join(dirname(abspath(__file__)), "assets/config.yaml")
-    with open(train_cfg_file, "r", encoding="utf-8") as f:
-        train_cfg = yaml.safe_load(f)
-        train_cfg["encoder_conf"].update(**model_config["encoder"])
-        train_cfg["decoder_conf"].update(**model_config["decoder"])
-
-    if isinstance(model_dir, str):
+    if not isinstance(model_dir, Path):
         model_dir = Path(model_dir)
 
-    model_file = model_dir / f"{model_name}.pt"
     download_model = True
-    disable_model_hash_check = kwargs.get("disable_model_hash_check", False)
-    if model_file.exists() and not disable_model_hash_check:
-        with open(model_file, "rb") as f:
+    model_ckpt_file = model_dir / f"{model_name}.pt"
+    if model_ckpt_file.exists():
+        with open(model_ckpt_file, "rb") as f:
             model_bytes = f.read()
+
         if hashlib.sha256(model_bytes).hexdigest() == MODELS[model_name]["sha256"]:
             download_model = False
         else:
-            model_file.unlink(missing_ok=True)
             logger.warning("model SHA256 chechsum mismatch, redownload model...")
 
-    if download_model and not disable_model_hash_check:
-        # Download model
-        model_dir.mkdir(exist_ok=True)
-        _download_from_modelscope(
+    if download_model:
+        model_dir.mkdir(parents=True, exist_ok=True)
+        modelscope.snapshot_download(
             model_id=MODELS[model_name]["model_id"],
             local_dir=model_dir,
-            allow_file_pattern=f"{model_name}.pt",
+            allow_file_pattern=None,
+            repo_type="model",
         )
 
-    model = DolphinSpeech2Text(
-        s2t_train_config=train_cfg,
-        s2t_model_file=model_file,
-        device=device,
-        task_sym=kwargs.get("task_sym", "<asr>"),
-        predict_time=kwargs.get("predict_time", True),
-        **kwargs,
-    )
+    assert (model_dir / "config.yaml").exists(), "model config not found, please redownload model!"
+    with open(model_dir / "config.yaml") as f:
+        configs = yaml.load(f, Loader=yaml.Loader)
+
+    model = init_speech_model(configs)
+    state_dict = torch.load(model_dir / f"{model_name}.pt", map_location="cpu")
+    model.load_state_dict(state_dict)
+    model = model.to(device)
+    model.eval()
+
+    model.model_configs = configs
+    model.device = device
+
     return model
-
-
-def _download_from_modelscope(model_id: str, local_dir: str, allow_file_pattern: str):
-    modelscope.snapshot_download(
-        model_id=model_id,
-        local_dir=local_dir,
-        allow_file_pattern=allow_file_pattern,
-        repo_type="model",
-    )
 
 
 def validate_lang_region(lang_sym: str, region_sym: str):
@@ -213,20 +163,24 @@ def validate_lang_region(lang_sym: str, region_sym: str):
     if all([lang_sym, region_sym]):
         if f"{lang_sym}-{region_sym}" not in LANGUAGE_REGION_CODES:
             raise Exception("Unsupport language or region!")
-    elif lang_sym and region_sym is None:
-        if lang_sym not in LANGUAGE_CODES:
-            raise Exception("Unsupport language!")
+    elif any([lang_sym, region_sym]):
+        if lang_sym is not None and region_sym is None:
+            assert lang_sym in LANGUAGE_CODES, "Unsupport language!"
+        elif lang_sym is None and region_sym is not None:
+            assert False, "If you specify a dialect, you must configure the language!"
 
     return True
 
 
 def transcribe_long(
-        model: DolphinSpeech2Text,
+        model: ASRModel,
         audio: str,
         lang_sym: str = None,
         region_sym: str = None,
         predict_time: bool = True,
         padding_speech: bool = False,
+        decoding_method: str = "attention_rescoring",
+        beam_size: int = 10,
         **kwargs,
     ) -> List[TranscribeSegmentResult]:
     """
@@ -236,9 +190,10 @@ def transcribe_long(
         model: model instance
         audio: audio path
         lang_sym: language symbol (e.g. zh)
-        region_sym: regiion symbol (e.g. CN)
+        region_sym: region symbol (e.g. CN)
         predict_time: whether predict timestamp (default: true)
-        padding_speech: whether padding speech to 30 seconds (default: false)
+        padding_speech: depreacted, whether padding speech to 30 seconds (default: false)
+        decoding_method: decoding methods, supports: attetion_rescoring (default), attention
 
     Returns:
         List[TranscribeSegmentResult]
@@ -249,11 +204,22 @@ def transcribe_long(
 
     logging.info("download vad model")
     vad_model_dir = Path(os.path.expanduser("~/.cache/dolphin/speech_fsmn_vad"))
-    vad_model_dir.mkdir(exist_ok=True)
-    _download_from_modelscope(VAD_MODEL, vad_model_dir, None)
+    vad_model_dir.mkdir(parents=True, exist_ok=True)
+    if not (vad_model_dir / "model.pt").exists():
+        modelscope.snapshot_download(
+            model_id=VAD_MODEL,
+            local_dir=vad_model_dir,
+            allow_file_pattern=None,
+            repo_type="model",
+        )
 
     logger.info("loading vad model")
-    vad_model = GenericFunASR(vad_model_dir, max_single_segment_time=SPEECH_LENGTH*1000, device="cpu")
+    vad_model = GenericFunASR(
+        vad_model_dir,
+        max_single_segment_time=SPEECH_LENGTH*1000,
+        device="cpu",
+        disable_update=True,
+    )
 
     # convert audio to sample rate 16k Mono channel audio
     tmp_audio = f"{audio}.wav"
@@ -263,17 +229,33 @@ def transcribe_long(
     segments = vad_model(input=tmp_audio, disable_pbar=True)[0]["value"]
 
     logger.info("decoding...")
+    tokenizer = init_tokenizer(model.model_configs)
+
     audio_segment = pydub.AudioSegment.from_wav(tmp_audio)
     for seg in segments:
         s, e = seg
-        raw_data = audio_segment[s:e].raw_data
-        waveform = np.frombuffer(raw_data, np.int16).flatten().astype(np.float32) / 32768.0
-        result = model(
-            speech=waveform,
-            lang_sym=lang_sym,
-            region_sym=region_sym,
-            predict_time=predict_time,
-            padding_speech=padding_speech,
+        raw_data = audio_segment[s:e].set_channels(1).raw_data
+        waveform: torch.Tensor = torch.frombuffer(raw_data, dtype=torch.int16).to(torch.float32) / 32768.0
+        batch = extract_feats([waveform.unsqueeze(0)], model.model_configs)
+        batch["feats"] = batch["feats"].to(model.device)
+        batch["feats_lengths"] = batch["feats_lengths"].to(model.device)
+
+        ret = model.decode(
+            methods=[decoding_method],
+            speech=batch["feats"],
+            speech_lengths=batch["feats_lengths"],
+            beam_size=beam_size,
+            infos={"tokenizer": tokenizer}
+        )
+        tokens = ret[decoding_method][0].tokens
+        nonspecial_tokens = _filter_nonspecial_tokens(tokens, tokenizer)
+        lang = tokenizer.ids2tokens([tokens[0]])[0][1:-1]
+        region = tokenizer.ids2tokens([tokens[1]])[0][1:-1]
+        result = TranscribeResult(
+            text=tokenizer.detokenize(tokens)[0],
+            text_nospecial=tokenizer.detokenize(nonspecial_tokens)[0],
+            language=lang,
+            region=region,
         )
 
         st = seconds_to_hms(s/1000)
@@ -293,13 +275,32 @@ def transcribe_long(
     return results
 
 
+def _filter_nonspecial_tokens(tokens: List[int], tokenizer: BaseTokenizer) -> List[int]:
+    """
+    Filter out special tokens from the token list.
+
+    Args:
+        tokens: list of token ids
+        tokenizer: tokenizer instance
+
+    Returns:
+        list of non-special token ids
+    """
+    last_time_id = tokenizer.tokens2ids(["<30.00>"])[0]
+    nonspecial_tokens = list(filter(lambda x: x > last_time_id , tokens))
+
+    return nonspecial_tokens
+
+
 def transcribe(
-        model: DolphinSpeech2Text,
+        model: ASRModel,
         audio: str,
         lang_sym: str = None,
         region_sym: str = None,
         predict_time: bool = True,
         padding_speech: bool = False,
+        decoding_method: str = "attention_rescoring",
+        beam_size: int = 10,
         **kwargs,
     ) -> TranscribeResult:
     """
@@ -309,26 +310,43 @@ def transcribe(
         model: model instance
         audio: audio path
         lang_sym: language symbol (e.g. zh)
-        region_sym: regiion symbol (e.g. CN)
+        region_sym: region symbol (e.g. CN)
         predict_time: whether predict timestamp (default: true)
-        padding_speech: whether padding speech to 30 seconds (default: false)
+        padding_speech: depreacted, whether padding speech to 30 seconds (default: false)
+        decoding_method: decoding methods, supports: attetion_rescoring (default), attention
 
     Returns:
         TranscribeResult
     """
-    waveform = load_audio(audio)
     validate_lang_region(lang_sym, region_sym)
 
     logger.info("decoding...")
-    result = model(
-        speech=waveform,
-        lang_sym=lang_sym,
-        region_sym=region_sym,
-        predict_time=predict_time,
-        padding_speech=padding_speech
+    batch = extract_feats([audio], model.model_configs)
+    batch["feats"] = batch["feats"].to(model.device)
+    batch["feats_lengths"] = batch["feats_lengths"].to(model.device)
+
+    tokenizer = init_tokenizer(model.model_configs)
+    ret = model.decode(
+        methods=[decoding_method],
+        speech=batch["feats"],
+        speech_lengths=batch["feats_lengths"],
+        beam_size=beam_size,
+        infos={"tokenizer": tokenizer}
     )
 
-    logger.info(f"decode result, rtf: {result.rtf}, language: {result.language}, region: {result.region}, text: {result.text}")
+    tokens = ret[decoding_method][0].tokens
+    nonspecial_tokens = _filter_nonspecial_tokens(tokens, tokenizer)
+    lang = tokenizer.ids2tokens([tokens[0]])[0][1:-1]
+    region = tokenizer.ids2tokens([tokens[1]])[0][1:-1]
+
+    result = TranscribeResult(
+        text=tokenizer.detokenize(tokens)[0],
+        text_nospecial=tokenizer.detokenize(nonspecial_tokens)[0],
+        language=lang,
+        region=region,
+    )
+
+    logger.info(f"decode result, language: {result.language}, region: {result.region}, text: {result.text}")
     return result
 
 
@@ -337,22 +355,15 @@ def cli():
 
     model = args.model
     if model not in MODELS:
-        logging.error(f"Unknown model {model}, Dolphin open source base, small model, please config the correct model.")
+        logging.error(f"Unknown model {model}, Dolphin open source {tuple(MODELS.keys())} model, please config the correct model.")
         return
 
-    model_dir = args.model_dir if args.model_dir else os.path.expanduser("~/.cache/dolphin")
+    model_dir = args.model_dir if args.model_dir else os.path.expanduser(f"~/.cache/dolphin/{model}")
     model_dir = Path(model_dir)
 
     logger.info("loading asr model")
     device = args.device if args.device else detect_device()
-    model_kwargs = {
-        "device": device,
-        "normalize_length": args.normalize_length,
-        "beam_size": args.beam_size,
-        "maxlenratio": args.maxlenratio,
-        "disable_model_hash_check": args.disable_model_hash_check,
-    }
-    model_instance = load_model(model, model_dir, **model_kwargs)
+    model_instance = load_model(model, model_dir, device)
 
     audio_duration = pydub.AudioSegment.from_file(args.audio).duration_seconds
     transcribe_fn = transcribe_long if audio_duration > SPEECH_LENGTH else transcribe
