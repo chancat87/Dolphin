f65604f77a9822496e3964f89ddd9a8825db58d1
yangyang <mengyangyang@dataoceanai.com>
2026-03-11 11:53:14 +0800

    add model registry module

---
+++ dolphin/model_registry.py
@@ -0,0 +1,15 @@
+
+MODELS = {
+    "base": {
+        "model_id": "DataoceanAI/dolphin-base",
+        "sha256": "688f0cdb26da2684a4eec200a432091920287585e8e332507cbe9c1ab6d77401",
+    },
+    "small": {
+        "model_id": "DataoceanAI/dolphin-small",
+        "sha256": "4a0c6c636657121ec2a2b656e97e45b29a8b29c92fa3998006e02ab146d8ac51",
+    },
+    "small.zh": {
+        "model_id": "DataoceanAI/dolphin-small-zh",
+        "sha256": "558a77994668afda64e230b1e10c4580940fc338bf150d310d577d2b4c109ec9",
+    }
+}
