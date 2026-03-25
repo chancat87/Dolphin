a1424bf70b7387466399da3d7242d6950fd95f29
yangyang <mengyangyang@dataoceanai.com>
2026-03-25 14:41:52 +0800

    compatible with v1 model feature extraction module

--- dolphin/processor.py
+++ dolphin/processor.py
@@ -1,10 +1,16 @@
+import copy
 from pathlib import Path
-from typing import List, Dict, Union
+from typing import List, Dict, Union, Optional
 
 import torch
 import torchaudio
 import numpy as np
 from torch.nn.utils.rnn import pad_sequence
+from torch_complex.tensor import ComplexTensor
+
+from dolphin.model import DefaultFrontend
+
+frontend = None
 
 
 def extract_feats(audios: List[Union[Path, torch.Tensor]], configs: Dict) -> Dict[str, torch.Tensor]:
@@ -26,9 +32,17 @@ def extract_feats(audios: List[Union[Path, torch.Tensor]], configs: Dict) -> Dic
             waveform = torchaudio.transforms.Resample(orig_freq=sample_rate, new_freq=16000)(waveform)
 
         # fbank feats
-        waveform = waveform * (1 << 15)
-        mel = torchaudio.compliance.kaldi.fbank(waveform=waveform, **configs["dataset_conf"]["fbank_conf"])
-        feats.append(mel)
+        if "frontend_conf" in configs["dataset_conf"]:
+            global frontend
+            if frontend is None:
+                frontend = DefaultFrontend(**configs["dataset_conf"]["frontend_conf"])
+                frontend.eval()
+            lengths = torch.tensor([waveform.size(-1)], dtype=torch.long)
+            feats.append(frontend(waveform, lengths)[0][0])
+        else:
+            waveform = waveform * (1 << 15)
+            mel = torchaudio.compliance.kaldi.fbank(waveform=waveform, **configs["dataset_conf"]["fbank_conf"])
+            feats.append(mel)
 
     feats = pad_sequence(feats, batch_first=True, padding_value=0.0)
     feats_lengths = torch.tensor([feat.size(0) for feat in feats], dtype=torch.long)
